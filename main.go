0d6db4c96b9fface26c6ac7350feced1cebdf1ea
ponyjackal <pony1125@outlook.com>
2023-06-18 17:46:19 +0330

    chore(env): add goDotEnv v1.5.1 for load environment file

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 }
